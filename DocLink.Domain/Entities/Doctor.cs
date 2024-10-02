7f73fe2137c71be15586dd591d37b6e2bcb7d093
SharaawyMohamed <sharawem585@gmail.com>
2024-10-02 03:02:31 +0300

    Add navication property from many side in doctor

--- DocLink.Domain/Entities/Doctor.cs
+++ DocLink.Domain/Entities/Doctor.cs
@@ -10,6 +10,9 @@
         public DateTime AppointmentDuration { get; set; }
 
         public ICollection<Appointment> Appointments { get; set; } = new HashSet<Appointment>();
+        public ICollection<LanguageSpoken> Languages { get; set; } = new HashSet<LanguageSpoken>();
+        public ICollection<Qualification> Qualifications { get; set; } = new HashSet<Qualification>();
+        public ICollection<Availability> Availabilities { get; set; } = new HashSet<Availability>();
 
         public string SpecialtyId { get; set; }
         public Specialty Specialty { get; set; }
