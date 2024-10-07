334c607809f2bc2f05e460168ab36b2efe110daa
Ali-nasser1 <popa0251@gmail.com>
2024-10-07 15:34:55 +0300

    connect to remote db

--- DocLink.APIs/Extensions/ApplicationServices.cs
+++ DocLink.APIs/Extensions/ApplicationServices.cs
@@ -21,7 +21,7 @@ namespace DocLink.APIs.Extensions
         {
             #region DbContext Registration
             Services.AddDbContext<DLDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(Configuration.GetConnectionString("RemoteConnection")));
             #endregion
 
             #region Identity User
