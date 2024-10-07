ebe9eba0fc9e4b9b2dd945d6ca7576754f814d0f
SharaawyMohamed <sharawem585@gmail.com>
2024-10-07 21:48:03 +0300

    Create Send Email Method Using FluentEmail for ForgetPassword

---
+++ DocLink.Domain/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿using DocLink.Domain.DTOs.ExternalDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocLink.Domain.Interfaces.Services
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(EmailDto email);
+
+    }
+}
---
+++ DocLink.Infrastructure/Extention/FluentEmailExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Mail;
+using System.Net;
+
+namespace DocLink.Infrastructure.Extention
+{
+    public static class FluentEmailExtensions
+    {
+        public static IServiceCollection AddFluentEmailServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var emailSettings = configuration.GetSection("Email");
+
+            var userName = emailSettings["UserName"];
+            var password = emailSettings["Password"];
+            var networkCredential=new NetworkCredential(userName, password);
+
+
+            var enableSsl = emailSettings.GetValue<bool>("EnableSSL");
+            var host = emailSettings["Server"];
+            var defaultFromEmail = emailSettings["SenderEmail"];
+            var useDefaultCredentials = emailSettings.GetValue<bool>("UseDefaultCredentials");
+            var port = emailSettings.GetValue<int>("Port");
+
+            var smtpClient = new SmtpClient
+            {
+                EnableSsl = enableSsl,
+                Host = host!,
+                Port = port,
+                UseDefaultCredentials = useDefaultCredentials,
+                Credentials = networkCredential,
+            };
+            services.AddFluentEmail(defaultFromEmail)
+                .AddSmtpSender(smtpClient);
+
+            return services;
+        }
+    }
+}
+#region smtpClient
+
+#endregion
