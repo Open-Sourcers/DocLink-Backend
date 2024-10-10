08ad23d8c09757eff3d1fb0bd73b66471aa09fda
Mohamed-Hussein-Dev <mohamd.h.yousuf@gmail.com>
2024-10-10 17:38:35 +0300

    add enum display name

--- DocLink.Domain/Enums/LoginProvider.cs
+++ DocLink.Domain/Enums/LoginProvider.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +10,9 @@ namespace DocLink.Domain.Enums
 {
     public enum LoginProvider
     {
+        [Display(Name = "Google")]
         Google = 1,
+        [Display(Name = "Facebook")]
         Facebook
     }
 }
